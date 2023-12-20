b108f5c3781aeef396ed5a69bc8baf9de200c7b7
cdavis1809 <133775062+cdavis1809@users.noreply.github.com>
2023-12-20 14:47:55 -0500

    added an alt to images

--- src/ImageResults.js
+++ src/ImageResults.js
@@ -10,7 +10,11 @@ export default function ImageResults(props) {
           return (
             <div className="col-6 mb-4" key={index}>
               <a href={photo.src.original} target="_blank" rel="noreferrer">
-                <img src={photo.src.landscape} className="img-fluid" />
+                <img
+                  src={photo.src.landscape}
+                  className="img-fluid"
+                  alt="definition display images"
+                />
               </a>
             </div>
           );
