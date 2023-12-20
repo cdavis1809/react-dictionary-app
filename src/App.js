2c22dbc2413afc3d61db69c0fdc61f8ff1c05b75
cdavis1809 <133775062+cdavis1809@users.noreply.github.com>
2023-12-20 15:43:47 -0500

    added a default search

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <div className="container">
         <header className="App-header"></header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="dog" />
         </main>
         <footer>
           Coded by:{" "}
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,21 +4,20 @@ import Results from "./Results.js";
 import ImageResults from "./ImageResults.js";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
+  let [loaded, setLoaded] = useState(false);
   let [results, setResults] = useState(null);
   let [imageResults, setImageResults] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data);
-    console.log(response.data);
   }
+
   function handleImageResponse(response) {
     setImageResults(response.data);
   }
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     let apiKey = "406035333ab93t5b036b8515eob03bf5";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
@@ -27,20 +26,38 @@ export default function Dictionary() {
     let imageApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${imageApiKey}`;
     axios.get(imageApiUrl).then(handleImageResponse);
   }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-  return (
-    <div className="dictionary">
-      <h1>Dictionary App</h1>
-      <h5>Please enter a search term:</h5>
-      <form onSubmit={search}>
-        <input type="search" autoFocus={true} onChange={handleKeywordChange} />
-      </form>
-      <br />
-      <hr />
-      <Results results={results} />
-      <ImageResults imageResults={imageResults} />
-    </div>
-  );
+
+  function load() {
+    setLoaded(true);
+    search();
+  }
+  if (loaded) {
+    return (
+      <div className="dictionary">
+        <h1>Dictionary App</h1>
+        <h5>Please enter a search term:</h5>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            autoFocus={true}
+            onChange={handleKeywordChange}
+          />
+        </form>
+        <br />
+        <hr />
+        <Results results={results} />
+        <ImageResults imageResults={imageResults} />
+      </div>
+    );
+  } else {
+    load();
+    return "loading...";
+  }
 }
